9f89d39b5d700148f70d8c6244b8d4fa9ecbe87e
Joun <urbanscratcher@gmail.com>
2024-05-06 18:03:44 +0900

    refactoring 3 functions

---
+++ w2/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+void printMenu()
+{
+  // 1 print help
+  std::cout << "1: Print help" << std::endl;
+
+  // 2 print eschange stats
+  std::cout << "2: Print exchange stats" << std::endl;
+
+  // 3 make an offer
+  std::cout << "3: Make an offer" << std::endl;
+
+  // 4 make a bid
+  std::cout << "4: Make a bid" << std::endl;
+
+  // 5 print wallet
+  std::cout << "5: Print wallet" << std::endl;
+
+  // 6 continue
+  std::cout << "6: Continue" << std::endl;
+
+  std::cout << "=================" << std::endl;
+}
+
+int getUserOption()
+{
+  int userOption;
+
+  std::cout << "Type in 1-6: ";
+  std::cin >> userOption;
+  std::cout << "You chose: " << userOption << std::endl;
+
+  return userOption;
+}
+
+void processUserOption(int userOption)
+{
+  if (userOption == 0) // bad input
+  {
+    std::cout << "Invalid choice. Choose 1-6" << std::endl;
+  }
+  if (userOption == 1)
+  {
+    std::cout << "Help - your aim is to make money, Analyze the market and make bids and offers" << std::endl;
+  }
+  if (userOption == 2)
+  {
+    std::cout << "Market looks good" << std::endl;
+  }
+  if (userOption == 3)
+  {
+    std::cout << "Mark and offer - enter the amount" << std::endl;
+  }
+  if (userOption == 4)
+  {
+    std::cout << "Make a bid - enter the amount" << std::endl;
+  }
+  if (userOption == 5)
+  {
+    std::cout << "Your wallet is empty" << std::endl;
+  }
+  if (userOption == 6)
+  {
+    std::cout << "Going to next time frame" << std::endl;
+  }
+}
+
+// Problem: every function after main() was not declared in the scope
+// Solution: declare the functions before main()
+int main()
+{
+  while (true)
+  {
+    printMenu();
+    int userOption = getUserOption();
+    processUserOption(userOption);
+  }
+  return 0;
+}
